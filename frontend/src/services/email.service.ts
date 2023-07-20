e2be9a804ef0ac9e649555c4c604470a2bc225b9
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2023-07-19 23:39:44 -0700

    Updated route for Lambda

--- frontend/src/services/email.service.ts
+++ frontend/src/services/email.service.ts
@@ -3,11 +3,11 @@ import { AiRequest } from '../models/airequest'
 import { Reply } from '../models/reply'
 
 // Local API route for FastAPI
-const API_URL = "http://127.0.0.1:8000"
+const API_URL = "https://ak52xf613f.execute-api.us-west-1.amazonaws.com/"
 
 class EmailService {
   async getEmailReply(request: AiRequest): Promise<Reply> {
-	const response = await axios.post(API_URL + '/get_reply', request);
+	const response = await axios.post(API_URL, request);
 
 	return response.data;
   }
