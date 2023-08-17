e277ba6c4b8dca825a67aba91d5d6528e9984dc3
Saurabh Kulkarni <srbhkulkarni13@gmail.com>
2023-08-16 21:28:44 -0700

    Dockerizing the app + updated API Gateway path

--- frontend/src/services/email.service.ts
+++ frontend/src/services/email.service.ts
@@ -3,18 +3,18 @@ import { AiRequest } from '../models/airequest'
 import { Reply } from '../models/reply'
 
 // Local API route for FastAPI
-const API_URL = "http://localhost:8000"
+const API_URL = "https://m65mtn0jel.execute-api.us-west-1.amazonaws.com/"
 
 class EmailService {
 	async getEmailReply(request: AiRequest): Promise<Reply> {
-		const response = await axios.post(API_URL+"/get_reply", request);
+		const response = await axios.post(API_URL + "/get_reply", request);
 		return response.data;
-	  }
+	}
 
-	  async checkService(): Promise<{message: string}> {
+	async checkService(): Promise<{ message: string }> {
 		const response = await axios.get(API_URL);
 		return response.data;
-	  }
+	}
 }
 
 export default new EmailService()
